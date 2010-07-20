cf7c05f18bc27a65675e3ff2ad511b57204b648e
Alex <maccman@gmail.com>
2010-07-20 10:50:34 -0700

    Rails beta 3 support

--- lib/hoptoad_notifier/railtie.rb
+++ lib/hoptoad_notifier/railtie.rb
@@ -3,19 +3,17 @@ require 'rails'
 
 module HoptoadNotifier
   class Railtie < Rails::Railtie
-    railtie_name :hoptoad_notifier
-
     rake_tasks do
       require "hoptoad_notifier/rails3_tasks"
     end
 
-    config.middleware.insert_after ActionDispatch::ShowExceptions, HoptoadNotifier::Rack
+    config.middlewares.insert_after ActionDispatch::ShowExceptions, HoptoadNotifier::Rack
 
     config.after_initialize do
       HoptoadNotifier.configure(true) do |config|
         config.logger           = Rails.logger
-        # config.environment_name = Rails.env
-        # config.project_root     = Rails.root
+        config.environment_name = Rails.env
+        config.project_root     = Rails.root
         config.framework        = "Rails: #{::Rails::VERSION::STRING}"
       end
     end
